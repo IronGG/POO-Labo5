eafaebc49a8c3875f2652729e4bd8f7e7d04ccbb
Yann Merk <yannmerk@gmail.com>
2022-11-13 13:39:33 +0100

    Added regular dimensions check + comments

--- src/Labo5/Matrice.java
+++ src/Labo5/Matrice.java
@@ -7,6 +7,13 @@ public class Matrice {
 
     //#region Constructors
 
+    /**
+     * Random matrix constructor
+     * Will create a matrix and fill it with values between 0 (included) and modulo
+     * @param length The width of each row of the matrix
+     * @param height The number of rows of the matrix
+     * @param modulo The max value possible in the matrix +1
+     */
     public Matrice(int length, int height, int modulo) {
 
         matrice = new int[length][height];
@@ -22,6 +29,13 @@ public class Matrice {
         checkModulo();
     }
 
+    /**
+     * Will create a matrix with already existing values
+     * The passed values must represent a non-empty, regular matrix
+     * It's values must be between 0 (included) and modulo
+     * @param matrice a matrix of int
+     * @param modulo The max value possible of the matrix +1
+     */
     public Matrice(int[][] matrice, int modulo) {
         this.matrice = matrice;
         this.modulo  = modulo;
@@ -29,6 +43,7 @@ public class Matrice {
         checkModulo();
     }
 
+    // Copy constructor
     public Matrice(Matrice matrice) {
         this.matrice = matrice.matrice;
         this.modulo  = matrice.modulo;
@@ -36,11 +51,15 @@ public class Matrice {
     }
 
     /**
-     * Throws runtime exception if matrix has an invalid value (less than 1 in size)
+     * Throws runtime exception if matrix is not a regular matrix
      */
     public void checkDimensions() {
         if (matrice.length == 0 || matrice[0].length == 0) {
             throw new RuntimeException("Invalid matrice size");
+        }
+        for(int i = 1; i < matrice.length; ++i) {
+            if (matrice[i].length != matrice[0].length)
+                throw new RuntimeException("matrice shape is not regular");
         }
     }
 
